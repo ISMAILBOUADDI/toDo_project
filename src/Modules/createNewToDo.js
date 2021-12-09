3741c42ba1b5e767fc519190eb0f3a74450ccdde
mohammed <mohammedkabli53@gmail.com>
2021-12-09 11:35:20 +0100

    class todo

---
+++ src/Modules/createNewToDo.js
@@ -0,0 +1,42 @@
+"use strict";
+
+class Todo {
+  constructor(title,description,dueDate,priority){
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+
+  setTitel(mytitle){
+    this.title = mytitle ;
+  }
+  
+  setDescription(mydescription){
+    this.description = mydescription;
+  }
+
+  setdueDate(mydueDate){
+    this.dueDate = mydueDate;
+  }
+
+  setPriority(myPriority){
+    this.priority = myPriority;
+  }
+
+  getTitel(mytitle){
+    return mytitle;
+  }
+
+  getDescription(mydescription){
+    return mydescription;
+  }
+
+  getdueDate(mydueDate){
+    return mydueDate;
+  }
+
+  getPriority(myPriority){
+    return myPriority;
+  }
+}
