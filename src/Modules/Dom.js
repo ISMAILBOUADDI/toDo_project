403648051060b89e8e5ee0bfb8f9e10f9c76e949
mmerguoum <merguoummourad@gmail.com>
2021-12-13 09:41:10 +0100

    affichage du projet

--- src/Modules/Dom.js
+++ src/Modules/Dom.js
@@ -29,3 +29,45 @@ const btnCancelEdit = document.querySelector('.cancel-edit-todo');
 
 const esTodayButton = document.querySelector('.is-today');
 const isThisWeekButton = document.querySelector('is-this-week');
+
+
+const renderProjects = (projObj) => {
+    const newProjectDiv = document.createElement('div');
+    const addedProjectWrapper = document.createElement('div');
+    const btnDelProject = document.createElement('span');
+    newProjectDiv.classList.add('added-project');
+    addedProjectWrapper.classList.add('added-project-wrapper');
+    addedProjectWrapper.innerHTML = `<i class="fas fa-tasks"></i><div>${projObj.getName()}</div>`;
+    btnDelProject.innerHTML = <i class="fas fa-times-circle btn-del-project"></i>;
+    newProjectDiv.append(addedProjectWrapper, btnDelProject);
+    projectsDiv.append(newProjectDiv);
+    updateProjectIndex();
+    saveToLocalStorage();
+  
+  };
+  
+  const selectActiveProject = () => {
+    const addedProjectWrappers = document.querySelectorAll(
+      '.added-project-wrapper'
+    );
+    addedProjectWrappers.forEach((project) => {
+      const projectName = project.querySelector('div').textContent;
+      project.addEventListener('click', () => {
+        clearDisplay(todosDiv);
+        activeProject = projectName;
+        projectTitle.textContent = activeProject;
+        section.classList.toggle('slide');
+        checkIfProjectExist();
+        renderTodoList();
+      });
+    });
+  };
+  
+  const updateProjectIndex = () => {
+    let projectIndex = 0;
+    const projects = document.querySelectorAll('.added-project');
+    projects.forEach((project) => {
+      project.setAttribute('data-project', `${projectIndex}`);
+      projectIndex++;
+    });
+  };
