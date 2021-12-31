74196a5b119e14e018cb63f1b49d355a5627b255
ismailbouaddi <ismailbouaddi55@gmail.com>
2021-12-31 22:57:19 +0100

    add fun todelete and updates

--- toDoWithRestApi/controllers.js
+++ toDoWithRestApi/controllers.js
@@ -47,6 +47,35 @@ class Controller {
        });
     }
 
-   
+    // updating 
+    async update(data,table,id) {
+        return new Promise((resolve, reject) => {
+            let query 
+            if (table == 'task') {
+                query = `UPDATE task SET titre='${data.titre}', status='${data.status }', description='${data.description}',date_db='${data.date_db}',date_fn='${data.date_fn}' WHERE id = ${id} `
+            }else if (table == 'project') {
+                query = `UPDATE project SET titre='${data.titre}', priorite='${data.priorite }', description='${data.description}',date_db='${data.date_db}',date_fn='${data.date_fn}' WHERE id = ${id} `
+            }
+            connection.query(query , function (err, result, fields) {
+             if (err) {
+               return reject(err);
+             }
+             resolve('Todo updated successfully');
+             });
+       });
+    }
+
+    // deleting 
+    async delete(table,id) {
+            return new Promise((resolve, reject) => {
+                connection.query(`DELETE FROM ${table} where id = ${id}`, function (err, result, fields) {
+                 if (err) {
+                   return   reject(`No todo with id ${id} found`);
+                 }
+                 resolve(`Todo deleted successfully`);
+                 });
+           });
+        
+    }
 }
 module.exports = Controller;
